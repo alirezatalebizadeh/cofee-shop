9882e15a27b485552a5fd54c362e778807437832
Alireza Talebizadeh <Alireza.talebizadeh78@gmail.com>
2024-02-03 09:15:28 +0330

    coding to show and hidden navbar

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,11 @@
 const toggleThemeBtns = document.querySelectorAll(".toggle-theme");
+let submenu = document.querySelector(".submenu");
+let submenuOpenBtn = document.querySelector(".submenu_open_btn");
+let nav = document.querySelector(".nav");
+let showNabBtn = document.querySelector(".show-nab-btn");
+let closeNabBtn = document.querySelector(".close-nab-btn");
+let overlay = document.querySelector(".overlay");
+
 toggleThemeBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
     document.documentElement.classList.remove("dark");
@@ -10,3 +17,24 @@ toggleThemeBtns.forEach((btn) => {
     }
   });
 });
+
+// !close navbar
+function closeNavbar() {
+  nav.classList.remove("right-0");
+  nav.classList.add("-right-64");
+  overlay.classList.remove("overlay--open");
+}
+
+submenuOpenBtn.addEventListener("click", (e) => {
+  e.currentTarget.parentElement.classList.toggle("text-orange-300");
+  submenu.classList.toggle("submenu-open");
+});
+
+showNabBtn.addEventListener("click", () => {
+  nav.classList.remove("-right-64");
+  nav.classList.add("right-0");
+  overlay.classList.add("overlay--open");
+});
+
+closeNabBtn.addEventListener("click", closeNavbar);
+overlay.addEventListener("click", closeNavbar);
