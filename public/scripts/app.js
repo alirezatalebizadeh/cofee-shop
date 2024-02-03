d58a70eeef79168139b720a4eb663ba5b5695a3a
Alireza Talebizadeh <Alireza.talebizadeh78@gmail.com>
2024-02-03 06:22:30 +0330

    styling submenu

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,12 @@
 const toggleThemeBtns = document.querySelectorAll(".toggle-theme");
-console.log(toggleThemeBtns);
 toggleThemeBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
+    document.documentElement.classList.remove("dark");
     if (localStorage.theme === "dark") {
-      document.documentElement.classList.remove("dark");
       localStorage.theme = "light";
     } else {
-      document.documentElement.classList.add("dark");
       localStorage.setItem("theme", "dark");
+      document.documentElement.classList.add("dark");
     }
   });
 });
