d3fa1ea8d9d46756d8a1ed4c1d2466d20bb7d63d
Alireza Talebizadeh <Alireza.talebizadeh78@gmail.com>
2024-02-03 23:39:29 +0330

    coding js to show and close cart create cart-menu

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,7 +5,12 @@ let nav = document.querySelector(".nav");
 let showNabBtn = document.querySelector(".show-nab-btn");
 let closeNabBtn = document.querySelector(".close-nab-btn");
 let overlay = document.querySelector(".overlay");
+// ! cart section
+let showCartBtn = document.querySelector(".show-cart-btn");
+let closeCartBtn = document.querySelector(".close-cart-btn");
+let cart = document.querySelector(".cart");
 
+//! dark and light mode
 toggleThemeBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
     document.documentElement.classList.remove("dark");
@@ -18,23 +23,57 @@ toggleThemeBtns.forEach((btn) => {
   });
 });
 
-// !close navbar
-function closeNavbar() {
-  nav.classList.remove("right-0");
-  nav.classList.add("-right-64");
-  overlay.classList.remove("overlay--open");
-}
-
 submenuOpenBtn.addEventListener("click", (e) => {
   e.currentTarget.parentElement.classList.toggle("text-orange-300");
   submenu.classList.toggle("submenu-open");
 });
 
+//! show cart and nav
+function showNavMenu(value) {
+  if (value === "cart") {
+    cart.classList.remove("-left-64");
+    cart.classList.add("left-0");
+    overlay.classList.add("overlay--open");
+    overlay.addEventListener("click", closeCart);
+  } else if (value === "nav") {
+    nav.classList.remove("-right-64");
+    nav.classList.add("right-0");
+    overlay.classList.add("overlay--open");
+    overlay.addEventListener("click", closeNavbar);
+  }
+}
+
+//! close nav and menu
+function closeNavMenu(val) {
+  if (val === "cart") {
+    cart.classList.remove("left-0");
+    cart.classList.add("-left-64");
+    overlay.classList.remove("overlay--open");
+    overlay.addEventListener("click", () => {
+      overlay.classList.remove("overlay--open");
+    });
+  } else if (val === "nav") {
+    nav.classList.remove("right-0");
+    nav.classList.add("-right-64");
+    overlay.classList.remove("overlay--open");
+    overlay.addEventListener("click", () => {
+      overlay.classList.remove("overlay--open");
+    });
+  }
+}
+
+//! show nav or cart
 showNabBtn.addEventListener("click", () => {
-  nav.classList.remove("-right-64");
-  nav.classList.add("right-0");
-  overlay.classList.add("overlay--open");
+  showNavMenu("nav");
+});
+showCartBtn.addEventListener("click", () => {
+  showNavMenu("cart");
 });
 
-closeNabBtn.addEventListener("click", closeNavbar);
-overlay.addEventListener("click", closeNavbar);
+//! close nav or cart
+closeNabBtn.addEventListener("click", () => {
+  closeNavMenu("nav");
+});
+closeCartBtn.addEventListener("click", () => {
+  closeNavMenu("cart");
+});
